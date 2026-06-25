ea5a4122ff09d0231a690b83b9c8c794fd97670e
cherrykiwimango <allensaju7@gmail.com>
2026-06-25 13:49:58 +0530

    Revert "feat: setup postgres database"
    This reverts commit d4b73f242fbda5ff6f5844f8cc7e8ecb6924130a.

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,6 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[8.1].define(version: 2026_06_10_024127) do
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "pg_catalog.plpgsql"
-
   create_table "posts", force: :cascade do |t|
     t.text "body"
     t.datetime "created_at", null: false
